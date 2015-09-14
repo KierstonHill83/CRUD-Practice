fb28dd3c6ee2b476638f52e1916c01a1fcc4a6a6
KierstonHill83 <khill1218@gmail.com>
2015-09-14 15:43:35 -0600

    Added routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 // *** main dependencies *** //
 //NEED these 2 lines when using dotenv//
-var env = require('dotenv');
+var dotenv = require('dotenv');
 dotenv.load();
 //NEED these 2 lines when using dotenv//
 var express = require('express');
@@ -14,6 +14,7 @@ var swig = require('swig');
 
 // *** routes *** //
 var routes = require('./routes/index.js');
+var llamas = require('./routes/api.js');
 
 
 // *** express instance *** //
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 
 // *** main routes *** //
 app.use('/', routes);
+app.use('/api/', llamas);
 
 
 // catch 404 and forward to error handler
---
+++ server/routes/api.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var Llama = require('../models/llamas.js');
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//GET ALL llamas
+router.get('/llamas', function(req, res, next) {
+  Llama.find({}, function(err, llamas) {
+    if (err) {
+      res.json({"message": err});
+    } else {
+      res.json(llamas);
+    }
+  });
+});
+
+//GET single llama
+router.get('/llama/:id', function(req, res, next) {
+  Llama.findById(req.params.id, function(err, llama) {
+    if (err) {
+      res.json({"message": err});
+    } else {
+      res.json(llamas);
+    }
+  });
+});
+
+//POST ALL llamas
+router.post ('/llamas', function(req, res, next) {
+  var newLlama = new Llama ({
+    name: req.body.name,
+    age: req.body.age,
+    spitter: req.body.spitter
+  });
+  newLlama.save(function(err, llama) {
+    if (err) {
+      res.json({"message": err});
+    } else {
+      res.json(llama);
+    }
+  });
+});
+
+//PUT single llama
+router.put('/llama/:id', function(req, res, next) {
+  Llama.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, llama) {
+    if (err) {
+      res.json({"message": err});
+    } else {
+      res.json(llama);
+    }
+  });
+});
+
+//DELETE single llama
+router.delete('/llama/:id', function(req, res, next) {
+  Llama.findByIdAndRemove(req.params.id, function(err, llama) {
+    if (err) {
+      res.json({"message": err});
+    } else {
+      res.json(llama);
+    }
+  });
+});
+
+module.exports = router;
